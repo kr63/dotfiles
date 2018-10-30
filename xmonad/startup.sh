b02d02b85caa43203fae399c519e6b8c199f3dd6
Rustam Karimov <rkarimov.r@gmail.com>
2018-10-30 20:28:09 +0400

    xmonad initial commit

---
+++ xmonad/startup.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+xsetroot -cursor_name left_ptr
+
+trayer\
+    --edge top\
+    --SetPartialStrut true\
+    --SetDockType true\
+    --height 24\
+    --transparent true\
+    --alpha 0\
+    --tint 0x0\
+    --distancefrom right\
+    --distance 435\
+    --widthtype request\
+    --expand true\
+    --align right &

9278ecd5a329d22067d509c20ef4d2fb9c4dba63
Rustam Karimov <rkarimov.r@gmail.com>
2019-05-09 16:22:54 +0400

    Rewrite config for pure xmonad

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -1,2 +1,24 @@
 #! /bin/sh
 
+#/usr/libexec/xfce-polkit &
+xsetroot -cursor_name left_ptr
+
+# start pulseaudio server & volume control
+start-pulseaudio-x11
+xfce4-volumed-pulse
+
+trayer\
+    --edge top\
+    --SetPartialStrut true\
+    --SetDockType true\
+    --height 24\
+    --transparent true\
+    --alpha 0\
+    --tint 0x0\
+    --distancefrom right\
+    --distance 435\
+    --widthtype request\
+    --expand true\
+    --align right &
+
+#systemctl stop --user evolution-source-registry.service
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,34 +1,100 @@
-import XMonad hiding ( (|||) )
-import XMonad.Config.Xfce
-import XMonad.Util.EZConfig(additionalKeys)
-
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Util.Replace(replace)
-
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
+import XMonad
 import qualified XMonad.StackSet as W
-
-import XMonad.Layout.LayoutCombinators
-import XMonad.Layout.LayoutScreens
-import XMonad.Layout.TwoPane
-import XMonad.Actions.PerWorkspaceKeys
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run            -- spawnPipe, hPutStrln
+import XMonad.Hooks.ManageDocks   -- avoidStruts, docksEventHook
+import XMonad.Hooks.DynamicLog    -- dynamicLogWithPP
+import XMonad.Hooks.ManageHelpers -- helper functions to be used in manageHook
+import XMonad.Hooks.SetWMName     -- setWMName
+import XMonad.Layout.PerWorkspace -- onWorkspace
 import XMonad.Layout.Circle
+import XMonad.Layout.TwoPane
 
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
+    xmonad $ def
+        { terminal = "urxvt"
+        , modMask = mod4Mask
+        , borderWidth = 2
+        , normalBorderColor  = "#353945"
+        , focusedBorderColor = "#ffffff"
+        , focusFollowsMouse = False
+        , clickJustFocuses = True
+        , workspaces  = myWorkspaces
+        , startupHook = myStartupHook
+        , manageHook = myManageHook <+> manageDocks <+> manageHook def
+        , layoutHook = avoidStruts $ myLayout
+        , handleEventHook = docksEventHook <+> handleEventHook def
+        , logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "grey" "" . shorten 50
+                    }
+        }
+        `additionalKeys` myKeys
+
+--------------------------------------------------------------------------------
+-- key bindigns
+myKeys =
+    [ (( mod4Mask, xK_e), spawn "thunar")
+    , (( mod4Mask, xK_o), spawn "emacsclient -c")
+    , (( mod4Mask, xK_p), spawn "rofi -show drun")
+    , (( mod4Mask, xK_s), spawn "xfce4-settings-manager")
+    , (( mod4Mask .|. shiftMask, xK_t), spawn "gnome-system-monitor")
+    , (( 0, xK_Print), spawn "xfce4-screenshooter")
+    , (( mod4Mask, xK_g), spawn "google-chrome-stable")
+    ]
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- layout & workspace definition
+ideaLayout = TwoPane (3/100) (1/2) ||| Full
+myLayout =
+    onWorkspace "9" (ideaLayout) $          -- ideaLayout will be used on workspace "9"
+    Tall 1 (0.03) (0.6) ||| Circle ||| Full -- layouts for the other workspaces
+myWorkspaces = ["1:web","2:files","3:emacs","4:lo","5:IM", "6:vm"] ++ map show [7..9]
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- auto startup
+myStartupHook = do
+    setWMName "LG3D"
+    spawn "~/.xmonad/startup.sh"
+    spawn "emacs --daemon"
+    spawn "xfsettingsd"
+    spawn "xfce4-volumed"
+    spawn "feh --no-xinerama --bg-fill /home/lxuser/software/wallpaper-2330974.jpg"
+    spawn "dbus-launch nm-applet"
+    spawn "urxvt"
+    spawn "emacsclient -c"
+    spawn "thunar"
+    spawn "google-chrome-stable"
+    spawn "compton"
+    spawn "alltray evolution"
+    -- spawn ""
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- Application manage hooks
 myManageHook = composeAll
                [ className                       =? "Xfce4-notifyd"                      --> doIgnore
                , className                       =? "URxvt"                              --> doRectFloat (W.RationalRect 0.2 0.2 0.7 0.7)
-               , className                       =? "Xfrun4"                             --> doCenterFloat
                , className                       =? "Wine"                               --> doCenterFloat
-               , className                       =? "Google-chrome"                      --> doShift "1"
-               , className                       =? "Thunar"                             --> doShift "2"
-               , className                       =? "Emacs"                              --> doShift "3"
-               , appName                         =? "libreoffice"                        --> doShift "4"
-               , title                           =? "LibreOffice"                        --> doShift "4"
-               , appName                         =? "VirtualBox"                         --> doShift "6"
-               , className                       =? "Skype"                              --> doShift "5"
+               , className                       =? "Google-chrome"                      --> doShift "1:web"
+               , className                       =? "Firefox"                            --> doShift "1:web"
+               , appName                         =? "Places" <&&> className =? "Firefox" --> doCenterFloat
+               , className                       =? "Thunar"                             --> doShift "2:files"
+               , className                       =? "Emacs"                              --> doShift "3:emacs"
+               , appName                         =? "libreoffice"                        --> doShift "4:lo"
+               , title                           =? "LibreOffice"                        --> doShift "4:lo"
+               , appName                         =? "VirtualBox"                         --> doShift "6:vm"
+               , className                       =? "Thunderbird"                        --> doShift "5:IM"
+               , className                       =? "Skype"                              --> doShift "5:IM"
+               , appName                         =? "Msgcompose"                         --> doCenterFloat
                , className                       =? "mpv"                                --> doCenterFloat
-               , className                       =? "GoldenDict"                         --> doCenterFloat
+               , className                       =? "vlc"                                --> doCenterFloat
+               , className                       =? "xmessage"                           --> doCenterFloat
+               , className                       =? "Gmpc"                               --> doCenterFloat
+               , className                       =? "Goldendict"                         --> doCenterFloat
                , className                       =? "Catfish"                            --> doCenterFloat
                , className                       =? "Xfce4-settings-manager"             --> doCenterFloat
                , className                       =? "Xfce4-appfinder"                    --> doCenterFloat
@@ -36,40 +102,15 @@ myManageHook = composeAll
                , className                       =? "Gedit"                              --> doCenterFloat
                , className                       =? "Evolution"                          --> doCenterFloat
                , className                       =? "Gnome-system-monitor"               --> doCenterFloat
+
+               , stringProperty "WM_ICON_NAME"   =? "Unlock Keyring"                     --> doCenterFloat
+               , stringProperty "WM_ICON_NAME"   =? "File Operation Progress"            --> doCenterFloat
+               , stringProperty "WM_ICON_NAME"   =? "Task Manager"                       --> doCenterFloat
+               , stringProperty "WM_ICON_NAME"   =? "Thunderbird Preferences"            --> doCenterFloat
+               , stringProperty "WM_WINDOW_ROLE" =? "CallWindow"                         --> doCenterFloat
+               , className                       =? "Claws-mail"                         --> doCenterFloat
+
+               , className =? "Claws-mail" <&&> stringProperty "WM_WINDOW_ROLE" =? "mainwindow"  --> doRectFloat (W.RationalRect 0.2 0.2 0.7 0.7)
+               , className =? "Xarchiver"  <&&> stringProperty "WM_ICON_NAME" =? "Extract files" --> doRectFloat (W.RationalRect 0.2 0.2 0.2 0.5)
+               , className =? "Xarchiver"                                                        --> doRectFloat (W.RationalRect 0.2 0.2 0.7 0.7)
                ]
-
-defaultLayouts = Tall 1 (0.03) (0.6) ||| Circle ||| Full
-myLayout = defaultLayouts
-
-myStartupHook = do
-    spawn "~/.xmonad/startup.sh"
-    spawn "xfce4-panel -r"
-    spawn "compton"
-    spawn "emacs --daemon"
-    spawn "urxvt"
-    spawn "thunar"
-    spawn "google-chrome-stable"
-    -- spawn ""
-
-main = do
-  replace
-  xmonad $ ewmh xfceConfig
-    { terminal = "urxvt"
-    , startupHook = myStartupHook
-    , modMask     = mod4Mask
-    , manageHook = myManageHook <+> manageDocks <+> manageHook xfceConfig
-    , borderWidth = 2
-    , normalBorderColor = "#353945"
-    , focusedBorderColor = "#ffffff"
-    , layoutHook = avoidStruts $ myLayout
-    }`additionalKeys`
-    [
-    (( mod4Mask, xK_p), spawn "xfce4-appfinder"),
-    (( mod4Mask, xK_e), spawn "thunar"),
-    (( mod4Mask, xK_o), spawn "emacsclient -c"),
-    (( mod4Mask, xK_s), spawn "xfce4-settings-manager"),
-    (( mod4Mask .|. shiftMask, xK_t), spawn "gnome-system-monitor"),
-    (( 0, xK_Print), spawn "xfce4-screenshooter"),
-    (( mod4Mask, xK_g), spawn "google-chrome-stable")
-    ]
-
